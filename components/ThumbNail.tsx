0be846fe9eae7559e79d0c4e5513f08f067e8022
juanichaide <jie_chaide@hotmail.com>
2023-11-28 21:00:53 -0300

    unavailable images render

--- components/ThumbNail.tsx
+++ components/ThumbNail.tsx
@@ -3,6 +3,7 @@ import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {ArtWorkItem} from '../models/entity';
 import {SingleArtworkScreenProps} from '../screens';
+import ErrorSVG from '../assets/error.svg';
 
 export const ThumbNail = ({item}: {item: ArtWorkItem}) => {
   const navigation = useNavigation<SingleArtworkScreenProps['navigation']>();
@@ -12,6 +13,8 @@ export const ThumbNail = ({item}: {item: ArtWorkItem}) => {
     navigation.navigate('SingleArtwork', {artWork: item});
   };
 
+  const imageURI = `https://www.artic.edu/iiif/2/${item?.image_id}/full/843,/0/default.jpg`;
+
   return (
     <TouchableOpacity style={styles.thumbNailContainer} onPress={handlePress}>
       <View style={styles.descriptionContainer}>
@@ -20,13 +23,20 @@ export const ThumbNail = ({item}: {item: ArtWorkItem}) => {
         <Text style={styles.artworkTitle}>{item?.title}</Text>
       </View>
       <View style={styles.imageContainer}>
-        <Image
-          source={{
-            uri: `https://www.artic.edu/iiif/2/${item?.image_id}/full/843,/0/default.jpg`,
-          }}
-          style={styles.image}
-          resizeMode="cover"
-        />
+        {item?.image_id ? (
+          <Image
+            source={{
+              uri: imageURI,
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        ) : (
+          <View style={[styles.image, styles.unavailableContainer]}>
+            <ErrorSVG height={100} />
+            <Text style={styles.unavailableText}>Unavailable</Text>
+          </View>
+        )}
       </View>
     </TouchableOpacity>
   );
@@ -73,5 +83,15 @@ export const styles = StyleSheet.create({
   },
   listContainer: {
     paddingHorizontal: 16,
+  },
+  unavailableContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3333',
+  },
+  unavailableText: {
+    color: '#ffff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
